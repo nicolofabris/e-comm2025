a24d8fb916e8da67111f9e734401c37761edec57
Nicolo Fabris <nicolo.fabris@inspirient.com>
2025-01-23 11:22:46 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
-import {useState} from "react";
+import { useState } from "react";
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
-import products from './db/data'
+import products from "./db/data";
+import Card from "./components/Card";
 
 function App() {
-	const [selectedCategory, setSelectedCategory] = useState(null)
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-	const [query, setQuery] = useState("")
+  const [query, setQuery] = useState("");
 
-	const handleInputChange = (e) => {
-		setQuery(e.target.value)
-	}
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
 
-	const filteredItems = products.filter(product => product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1))
+  const filteredItems = products.filter((product) =>
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+  );
 
-	const handleChange = (e) => {
-		setSelectedCategory(e.target.value)
-	}
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
 
-	
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(({ img, title, star, reviews,newPrice, prevPrice }) => (
+      <Card
+        key={Math.random()}
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+        newPrice={newPrice}
+      />
+    ));
+  }
+
+  const result = filteredData(products, selectedCategory, query)
+
   return (
     <>
       <Sidebar />
