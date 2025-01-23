56e13a4028690cbf1bc30611ec1df5f4c2237d1e
Nicolo Fabris <nicolo.fabris@inspirient.com>
2025-01-23 12:25:04 +0100

    refactor

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,11 +1,56 @@
 import Input from "../../components/Input";
 import "./Colors.css";
 
-const Colors = () => {
+const Colors = ({ handleChange }) => {
   return (
     <div>
       <h2 className="sidebar-title color-title">Colors</h2>
-      <Input />
+      <label className="sidebar-label-container">
+        <input type="radio" onChange={handleChange} value="" name="test2" />
+        <span className="checkmark all"></span>All
+      </label>
+
+      <Input
+        handleChange={handleChange}
+        value="black"
+        title="Black"
+        name="test1"
+        color="black"
+      />
+      <Input
+        handleChange={handleChange}
+        value="blue"
+        title="Blue"
+        name="test1"
+        color="blue"
+      />
+      <Input
+        handleChange={handleChange}
+        value="red"
+        title="Red"
+        name="test1"
+        color="red"
+      />
+      <Input
+        handleChange={handleChange}
+        value="green"
+        title="Green"
+        name="test1"
+        color="green"
+      />
+      <label className="sidebar-label-container">
+        <input
+          type="radio"
+          onChange={handleChange}
+          value="white"
+          name="test1"
+        />
+        <span
+          className="checkmark"
+          style={{ background: "white", border: "2px solid black" }}
+        ></span>
+        White
+      </label>
     </div>
   );
 };
